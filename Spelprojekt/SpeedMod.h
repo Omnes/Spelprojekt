5ceade11ec2d9966419ba777516f432f5082012e
Robin Iderström <robin.iderstrom@live.se>
2013-01-24 14:58:24 +0100

    mfing filters

---
+++ Spelprojekt/SpeedMod.h
@@ -0,0 +1,23 @@
+//#ifndef SPEEDMOD
+//#define SPEEDMOD
+//
+//#include <SFML\Window\
+//
+//class SpeedMod{
+//public:
+//	SpeedMod(float time);
+//	~SpeedMod();
+//
+//private:
+//	float mAliveTime;
+//	sf::Clock
+//
+//
+//
+//};
+//
+//
+//
+//#endif
+
+//kanske bäst att skippa denna...
