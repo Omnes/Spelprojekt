25e40d651697c1d46d64e9f4542905930c631f0d
Jessica Lee <jessicall_2@msn.com>
2013-01-28 16:54:41 +0100

    hej

--- Spelprojekt/Entity.h
+++ Spelprojekt/Entity.h
@@ -1,7 +1,7 @@
 #ifndef ENTITY
 #define ENTITY
-
 #include <SFML\Graphics\Sprite.hpp>
+#include "SpeedMod.h"
 
 class Entity{
 public:
@@ -11,6 +11,7 @@ public:
 	virtual sf::Sprite* getSprite()=0;
 	virtual bool getAlive()=0;
 	virtual void collide(Entity* entity)=0;
+	virtual void setMod(SpeedMod* speedMod)=0;
 
 	//animation
 	//etc...
