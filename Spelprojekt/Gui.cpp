db4ecc522d40b4901d7f9d64241976cab12f9dbe
Jessica Lee <jessicall_2@msn.com>
2013-02-11 17:53:24 +0100

    gggf

--- Spelprojekt/Gui.cpp
+++ Spelprojekt/Gui.cpp
@@ -2,10 +2,12 @@
 #include "ResourceManager.h"
 #include "RemoveObstacleButton.h"
 #include "WindowManager.h"
+#include "TacticalVisionButton.h"
 
 Gui::Gui(): mWindow(WindowManager::getInst().getWindow()){
 	mButtons.push_back(new RemoveObstacleButton("Stone",sf::Vector2f(0,100),"knapp1.jpg",this));
 	mButtons.push_back(new RemoveObstacleButton("Gren",sf::Vector2f(0,250),"knapp2.jpg",this));
+	mButtons.push_back(new TacticalVisionButton(sf::Vector2f(0,400),"knapp1.jpg",this));
 	mGuiSprite.setTexture(*ResourceManager::getInst().getTexture("Resources/Misc/Ui.png"));
 	mGuiSprite.setPosition(sf::Vector2f(0,720-72));
 }
