1043eef70c9c60623bef1f2e94b3dc0e25835fec
Sean <conk3r_xd@hotmail.com>
2013-03-04 17:11:50 +0100

    asdasd

---
+++ Spelprojekt/Cutscene.h
@@ -0,0 +1,39 @@
+#ifndef CUTSCENE
+#define CUTSCENE
+#include "States.h"
+#include <vector>
+#include <SFML\Graphics\Sprite.hpp>
+
+class Scene;
+
+class Cutscene : public States{
+public:
+	Cutscene(std::string filepath);
+	~Cutscene();
+
+	void update();
+	void render();
+
+	void loadFromFile(std::string file);
+	void goToNext();
+
+	std::string getMusic();
+
+private:
+	typedef std::vector<Scene*> SceneVector;
+	typedef std::pair<SceneVector,sf::Sprite*> Enviroment;
+
+	typedef std::vector<Enviroment> Enviroments;
+	Enviroments mEnviroments;
+
+	int mCurrentEnviroment;
+	int mCurrentScene;
+
+	std::string mMusic;
+
+};
+
+
+
+
+#endif
